3fa881c197add1d09c749d938822127a9618eecc
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-02-03 23:41:56 +0300

    BanzHow

---
+++ BanzHow/Controllers/BlogController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanzHow.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            //veritabanından bütün blog bilgilerini al ve View üzerine gönder.
+            //kendisine gelen blog bilgilerini dinamik içerik üreten View yapısı bunu statik HTML'e çevirir.
+            //bloglar
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
